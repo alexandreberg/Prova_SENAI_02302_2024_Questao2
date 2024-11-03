0ccc6f24c63cf7b39215c593e57b928b8e74436e
alexandreberg <alexandreberg@gmail.com>
2024-11-03 13:45:29 -0300

    Adic. setup.py comentado

---
+++ ros2-humble_container/ros2_ws_sensor_sim/ros2_ws/src/sensor_sim/setup_comentado.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup  # Importa funções para encontrar pacotes e configurar o pacote
+
+package_name = 'sensor_sim'  # Define o nome do pacote
+
+setup(
+    name=package_name,  # Nome do pacote
+    version='0.0.0',  # Versão do pacote
+    packages=find_packages(exclude=['test']),  # Encontra todos os pacotes, exceto a pasta 'test'
+    data_files=[
+        # Define arquivos adicionais a serem incluídos
+        ('share/ament_index/resource_index/packages',  # Caminho para o índice de recursos do pacote
+            ['resource/' + package_name]),  # Inclui a pasta 'resource' do pacote
+        ('share/' + package_name, ['package.xml']),  # Inclui o arquivo 'package.xml'
+    ],
+    install_requires=['setuptools'],  # Dependências do pacote
+    zip_safe=True,  # Indica se o pacote pode ser instalado como um arquivo zip
+    maintainer='rosuser',  # Nome do mantenedor
+    maintainer_email='rosuser@todo.todo',  # Email do mantenedor
+    description='TODO: Package description',  # Descrição do pacote (a ser completada)
+    license='TODO: License declaration',  # Licença do pacote (a ser completada)
+    tests_require=['pytest'],  # Dependências para testes
+    entry_points={  # Define pontos de entrada para scripts executáveis
+        'console_scripts': [  # Define scripts que podem ser executados no terminal
+            'sensor_node = sensor_sim.sensor_node:main',  # Cria um script 'sensor_node' que executa a função 'main' do módulo 'sensor_node'
+        ],
+    },
+)
